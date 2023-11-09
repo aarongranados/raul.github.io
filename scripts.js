f6d00700e86dd430214db6d6ddfa76767419bcb9
aarongranados <50938494+aarongranados@users.noreply.github.com>
2023-11-08 23:43:07 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('pedidoForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Lógica para enviar el pedido por correo o realizar acciones adicionales
+
+    alert('Pedido enviado correctamente. Nos pondremos en contacto contigo pronto.');
+});
